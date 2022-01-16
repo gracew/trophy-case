857779c1e5e6ecb0594287e6d5bdbdddd885cc71
Grace Wang <gracewang92@gmail.com>
2022-01-15 16:18:13 -0800

    add connect wallet to index page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Image from "next/image";
 import { useEffect, useState } from "react";
-import { Nav } from "react-bootstrap";
+import { Button, Nav } from "react-bootstrap";
+import Web3Modal from "web3modal";
 import Badge from "../components/badge";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   const [badges, setBadges] = useState([]);
+  const [address, setAddress] = useState();
 
   useEffect(() => {
     fetch('https://deep-index.moralis.io/api/v2/0x06e6f7d896696167b2da9281ebaf8a14580fbfcc/nft?chain=mumbai&format=decimal', {
@@ -18,19 +20,34 @@ export default function Home() {
       .then(parsed => setBadges(parsed.result));
   }, [])
 
+  useEffect(() => {
+    const { ethereum } = window;
+    if (ethereum && ethereum.selectedAddress) {
+      // setAddress(ethereum.selectedAddress);
+    }
+  });
+
+  async function connectWallet() {
+    const web3Modal = new Web3Modal({ providerOptions: {} });
+    const connection = await web3Modal.connect();
+    setAddress(connection.selectedAddress);
+  }
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <div className={styles.mainUserContainer}>
-          <div>
-            <div className={styles.mainUserImageContainer}>
-              <Image src="/images/mcallister.png" layout="fill" objectFit="contain" />
+        {!address && <Button onClick={connectWallet} style={{ marginTop: 50 }}>Connect Wallet</Button>}
+        {address &&
+          <div className={styles.mainUserContainer}>
+            <div>
+              <div className={styles.mainUserImageContainer}>
+                <Image src="/images/mcallister.png" layout="fill" objectFit="contain" />
+              </div>
+              <h1 className={styles.title}>sammy.eth</h1>
             </div>
-            <h1 className={styles.title}>sammy.eth</h1>
-          </div>
-        </div>
+          </div>}
 
-        <div>
+        {address && <div>
           <Nav className={styles.nav}>
             <Nav.Item>
               <Nav.Link>My Badges</Nav.Link>
@@ -40,9 +57,13 @@ export default function Home() {
             </Nav.Item>
           </Nav>
           <div className={styles.grid}>
-            {badges.filter(badge => badge.metadata).map(badge => <Badge key={badge.token_id} contractName={badge.name} metadata={badge.metadata} />)}
+            {badges.filter(badge => badge.metadata).map(badge =>
+              <a href={`https://testnets.opensea.io/assets/mumbai/${badge.token_address}/${badge.token_id}`}>
+                <Badge key={badge.token_id} contractName={badge.name} metadata={badge.metadata} />
+              </a>
+            )}
           </div>
-        </div>
+        </div>}
       </main>
     </div>
   );

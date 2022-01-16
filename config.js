3642de74bd66b0038b3641edd1575504d2b35fb8
jw122 <thejuliawu@gmail.com>
2022-01-15 19:56:16 -0800

    fix order

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const duolingoNftAddress = "0x2825fecd39f007cb82df9b354c517f6431b0bddd";
-export const appleNftAddress = "0x8c3c8483046f717134f79d4Bc459f12CbFcED73D";
+export const appleNftAddress = "0x2825fecd39f007cb82df9b354c517f6431b0bddd";
+export const duolingoNftAddress = "0x8c3c8483046f717134f79d4Bc459f12CbFcED73D";
